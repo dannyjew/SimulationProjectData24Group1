bf14c0c6f47e119dd0872e9785e6bee706f59e92
Matt <91061879+MLyons97@users.noreply.github.com>
2021-09-28 16:37:58 +0100

    UPDATE: Testing function working plus some touch-ups

---
+++ Project/test_File.py
@@ -0,0 +1,20 @@
+import SImulation_Class as S
+import Trainees as T
+import TrainingCentre as TC
+
+
+Testing_Tom = T.Trainees("Training Towers", name="Testing Tom")
+Testing_Towers = TC.TrainingCentre("Training Towers")
+Simulation_Instance = S.Simulation("State", (1, 2, 3, 4), True)
+
+
+def test_trainee_name_getter():
+    assert Testing_Tom.Name == "Testing Tom"
+
+
+def test_course_length_getter():
+    assert Testing_Tom.Course_length == 3
+
+
+def test_capacity():
+    assert Testing_Towers.Capacity == 100
